e4b240fa018cc2b7aadcbd20bc7cceaf8652aba3
Taran.Soft <taran.soft@gmail.com>
2026-01-23 19:29:56 +0100

    add properties to UserCreated event

--- GarageSpace.Contracts/UserCreated.cs
+++ GarageSpace.Contracts/UserCreated.cs
@@ -4,5 +4,7 @@
     {
         public long UserId { get; set; }
         public required string Name { get; set; }
+        public required string Nickname { get; set; }
+        public required string Email { get; set; }
     }
 }
